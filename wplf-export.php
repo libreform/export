8a141b99d71d3c93ca8b83159cc633c93bcc93ed
Viljami Kuosmanen <viljami@avoinsorsa.fi>
2017-10-24 21:37:48 +0300

    Fix license in main file, add copyright notice

--- wplf-export.php
+++ wplf-export.php
@@ -6,9 +6,22 @@
  * Version: 1.0.1
  * Author: @k1sul1
  * Author URI: https://github.com/k1sul1
- * License: GPLv2
+ * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl.html
  * Text Domain: wplf-export
+ */
+
+/** Copyright 2017 Christian Nikkanen
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 3, as
+  published by the Free Software Foundation.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 class WPLF_Export {
